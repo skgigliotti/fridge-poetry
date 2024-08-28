c68cd4042ee904985f507732588e0f595c82791c
Sophia <sophia@winnie.com>
2024-08-28 00:15:17 -0700

    Fix dragging

--- src/fridge/DropZone.tsx
+++ src/fridge/DropZone.tsx
@@ -1,28 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import './DropZone.css'; 
+import Word from "./Word";
 
+let words: Array<string> = ['write', 'a', 'poem']
 function DropZone() {
-    const [ dragOver, setDragOver ] = React.useState(false); 
+    const [dragOver, setDragOver] = useState(false); 
+    const [droppedWords, setWords] = useState(words || []);
     const handleDragOverStart = () => setDragOver(true);
     const handleDragOverEnd = () => setDragOver(false);
+    const handleDrop = (event: React.DragEvent<HTMLElement>) => {
+        const id = event.dataTransfer.getData('text');
+        setWords([...droppedWords, id]);
+    }
     return (
         <div 
         onDragOver={enableDropping}
         onDrop={handleDrop}
         onDragEnter={handleDragOverStart}
         onDragLeave={handleDragOverEnd}
-        className={ dragOver ? 'hovering' : '' }>
-            Drop Area
+        className={ dragOver ? 'hovering drop-zone' : 'drop-zone' }>
+        {
+          droppedWords.map(function(word, i) {
+            return <Word word={word} key={i}/>;
+          })
+        }
         </div>
     );
 }
   
 const enableDropping = (event: React.DragEvent<HTMLElement>) => { 
     event.preventDefault();
-}
-const handleDrop = (event: React.DragEvent<HTMLElement>) => {
-    const id = event.dataTransfer.getData('text');
-    console.log(`Somebody dropped an element with id: ${id}`);
 }
   
 export default DropZone;
