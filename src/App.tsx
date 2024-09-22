6c21e49627bfa7a4ace54f1998bcb275ace638bc
Sophia <sophia@winnie.com>
2024-09-22 11:25:15 -0700

    Add words and remove headers

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,16 @@ import Portfolio from './Portfolio';
 import { Flex } from 'antd';
 
 let my_words = [
-  'welcome', 'shout', 'the',  'hello', 'world', 'foo', 'a', 'cats', 'beauty', 'programmatically', 'joy', 'peaceful',
-  'tomorrow', 'whirlwind', 'oneiric', 'sunshine', 'perplexing', 'and', 'nor', 'whisper', 'flowers',
-  'sing', 'grow', 'stars', 'computers', 'today', 'dark', 'glow', 'dance', 'delicious', 'run',
-  'an', 'that', 'wonder', 'yesterday', 'distracted', 'imminent'
+  'welcome', 'shout', 'the',  'hello', 'world', 'foo', '?', 'a', 'cats', 'beauty', 'programmatically', 'joy', 'peaceful',
+  'tomorrow', 'whirlwind', 'oneiric', 'in', 'sunshine', '.', 'perplexing', 'on', 'and', 'nor', 'whisper', 'flowers',
+  'sing', 'grow', 'stars', 'at', 'computers', 'today', 'dark', 'glow', 'dance', 'delicious', 'run',
+  'an', 'that', 'wonder', 'to', 'yesterday', 'with', 'distracted', 'imminent', 'for', '!', 'very', 'friend', 'by'
 ]
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Word word={'Sophia'} fontSize='36px'/>
-        <Word word={'Gigliotti'} fontSize='36px'/>
-      </header>
-      <Portfolio/>
       <Flex wrap style={{ maxWidth: "100%", padding: "2em"}} vertical={false}>
         {my_words.map(function(word, i){
             return <Word word={word} key={i}/>;}
